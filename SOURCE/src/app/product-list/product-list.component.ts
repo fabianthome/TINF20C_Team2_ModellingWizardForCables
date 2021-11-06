5f5ae0afce55b19acdfd6a86bf2685c6ea659d5b
Thorsten Rausch <thorsten.rausch@it-designers.de>
2021-11-06 21:26:59 +0100

    Add placeholder image

---
+++ SOURCE/src/app/product-list/product-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-table',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  title = 'All Products'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
