f2db38f36b5d2dff9405b37f43022b0d506b23ed
Thorsten Rausch <thorsten.rausch@it-designers.de>
2021-11-06 21:25:36 +0100

    Add a product list with placeholder products

--- SOURCE/src/app/app.module.ts
+++ SOURCE/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule
