db25aefb90e6ff1d956d65d126cdedf41fbb1319
Thorsten Rausch <thorsten.rausch@it-designers.de>
2021-11-08 15:18:55 +0100

    Rename to CableWizard

--- SOURCE/karma.conf.js
+++ SOURCE/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/SOURCE'),
+      dir: require('path').join(__dirname, './coverage/CableWizard'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- SOURCE/src/app/app.component.spec.ts
+++ SOURCE/src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
+  beforeEach(async() => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SOURCE'`, () => {
+  it(`should have as title 'CableWizard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SOURCE');
+    expect(app.title).toEqual('CableWizard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SOURCE app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CableWizard app is running!');
   });
 });
