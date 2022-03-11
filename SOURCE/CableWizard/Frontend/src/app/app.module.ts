44863922fc553b4115be7c08bb1aeeb2434e4d31
kevinpauer <kevinpauer@t-online.de>
2022-03-11 10:18:00 +0100

    Restructuring

---
+++ SOURCE/CableWizard/Frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- SOURCE/src/app/connectors.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from "./product";
-
-export const CONNECTORS: Product[] = [
-  { name: "USB Type A Connector", description: "This is a placeholder" },
-  { name: "USB Type B Connector", description: "This is also a placeholder" },
-  { name: "USB Type C Connector", description: "This is a descriptive description" },
-  { name: "USB Mini Connector", description: "This is a placeholder" },
-  { name: "USB Micro Connector", description: "This is a placeholder" },
-];
