b69096d21cd21c8a3c62df6fa120b788941fd0dd
Leon Amtmann <leon@amtmann.de>
2022-03-19 11:59:10 +0100

    Added ability to get xml file from file system and return it as parsed JSON string.

--- SOURCE/CableWizard/Backend/app.py
+++ SOURCE/CableWizard/Backend/app.py
@@ -1,8 +1,9 @@
 from os.path import isfile, join
 
-from flask import Flask, redirect
+from flask import Flask, request
 from os import listdir
 import json
+import xmltodict
 
 app = Flask(__name__)
 
@@ -30,13 +31,30 @@ def list_to_string(s):
     return str1
 
 
-# cable files may only start with "cable"
+#cable files may only start with "cable"
 @app.route('/getcablefilelist')
 def get_cable_file_list():
     mypath = "./data/cables"
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     onlycables = [x for x in onlyfiles if (x[:5] == "cable")]
     return json.dumps(onlycables)
+
+
+#pass cable name in parameter "cablename"
+#curl -XGET "localhost:5000/getcable?cablename=cable_caex_class_model.xml"
+@app.route('/getcable')
+def get_cable():
+    cablename = request.args.get('cablename')
+    print("get_cable() You requested cable " + cablename)
+    cable_file_path = "./data/cables/" + cablename
+
+    file = open(cable_file_path, 'r')
+    content = file.read()
+
+    o = xmltodict.parse(content)
+    jsonxml = json.dumps(o)
+
+    return jsonxml
 
 
 if __name__ == '__main__':
