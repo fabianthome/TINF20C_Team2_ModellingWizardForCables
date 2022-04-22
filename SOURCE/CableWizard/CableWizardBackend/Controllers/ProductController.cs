77ddb44b82453eff99482ecbb7e73f0fbf23347a
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-04-22 11:01:28 +0200

    add aml engine

---
+++ SOURCE/CableWizard/CableWizardBackend/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using CableWizardBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CableWizardBackend.Controllers;
+
+[ApiController]
+[Route("api/v2/products")]
+public class ProductController : Controller
+{
+    [HttpGet(Name = "GetProductInfos")]
+    public IEnumerable<ProductInfo> Get()
+    {
+        var products = new[]
+        {
+            new ProductInfo
+            {
+                Id = "1",
+                Name = "example-cable",
+                Producer = "Balluff"
+            }
+        };
+        return products;
+    }
+}
---
+++ SOURCE/CableWizard/CableWizardBackend/Models/ProductInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace CableWizardBackend.Models;
+
+public class ProductInfo
+{
+    public string Id;
+    public string Name;
+    public string Producer;
+}
