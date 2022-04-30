a70b876f44be87580e8c81ef9b04fe4817b83ced
Thorsten Rausch <thorsten.rausch@it-designers.de>
2022-04-30 13:06:47 +0200

    make id parameter string

--- SOURCE/CableWizard/Backend/Controllers/ProductDetailsController.cs
+++ SOURCE/CableWizard/Backend/Controllers/ProductDetailsController.cs
@@ -4,12 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace CableWizardBackend.Controllers;
 
 [ApiController]
-[Route("api/v2/product-details/{id:int}")]
+[Route("api/v2/product-details/{id}")]
 public class ProductDetailsController : Controller
 {
     [HttpGet(Name = "GetProductDetails")]
     
-    public ProductDetails Get(int id)
+    public ProductDetails Get(string id)
     {
         AmlSerializer.GetProductDetails();
         return new ProductDetails
