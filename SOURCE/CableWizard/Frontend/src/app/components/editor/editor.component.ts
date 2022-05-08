1d293a312e0dbdf805501446a6383fc2d5153b70
kevinpauer <kevinpauer@t-online.de>
2022-05-08 14:44:55 +0200

    Fix one more bug lol

--- SOURCE/CableWizard/Frontend/src/app/components/editor/editor.component.ts
+++ SOURCE/CableWizard/Frontend/src/app/components/editor/editor.component.ts
@@ -67,22 +67,24 @@ export class EditorComponent implements OnInit, OnDestroy {
         console.log(this.standardTypeFemale);
         this.standardRouteFemale = this.cable.connectors[1].path;
         console.log(this.cable);
+
+        this.dataService.getPossibleConnectors().subscribe((res) => {
+          this.possibleConnectors = res;
+          this.possibleConnectors.forEach((element) => {
+            if (element.item1 == this.standardTypeMale) {
+              this.standardRouteMale = element.item2;
+              console.log(this.standardRouteMale);
+            }
+          });
+          this.possibleConnectors.forEach((element) => {
+            if (element.item1 == this.standardTypeFemale) {
+              this.standardRouteFemale = element.item2;
+              console.log(this.standardRouteFemale);
+            }
+          });
+        });
       });
 
-    this.dataService.delay(1000);
-    this.dataService.getPossibleConnectors().subscribe((res) => {
-      this.possibleConnectors = res;
-      this.possibleConnectors.forEach((element) => {
-        if (element.item1 == this.standardTypeMale) {
-          this.standardRouteMale = element.item2;
-        }
-      });
-      this.possibleConnectors.forEach((element) => {
-        if (element.item1 == this.standardTypeFemale) {
-          this.standardRouteFemale = element.item2;
-        }
-      });
-    });
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map((value) => (typeof value === 'string' ? value : value.name)),
